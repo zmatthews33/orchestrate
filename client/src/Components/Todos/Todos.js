1f03a1966c2efce5979d87ea943d1b17f2744429
Weslley Braga <wesbragagt@gmail.com>
2019-06-19 00:09:02 -0500

    todo list crud functionality

--- client/src/Components/Todos/Todos.js
+++ client/src/Components/Todos/Todos.js
@@ -25,16 +25,54 @@ export class Todos extends Component {
             todos: [newTodo, ...this.state.todos]
         });
     }
+
+    markCompleted(id) {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            })
+        });
+
+        setTimeout(() => {
+            this.setState({
+                todos: this.state.todos.filter(todo => todo.id !== id)
+            })
+        }, 1000);
+    }
     render() {
         return (
-            <div>
-                <input
+            <div className="todos">
+                <div className="todos-form">
+                <input className="todos-input"
                     type="text"
                     placeholder="add todo..."
                     value={this.state.userInput}
                     onChange={e => this.handleInputChange(e.target.value)}
                 />
-                <button onClick={() => this.addTodo()}>Add</button>
+                <button className="todos-add" onClick={() => this.addTodo()}>Add</button>
+                </div>
+                <ul className="todos-list-group">
+                    {this.state.todos.map(todo => {
+                        if (todo.completed) {
+                            return (
+                                <li className="todos-list-group-item" key={todo.id}>
+                                    <s>{todo.value}</s>
+                                    <button className="todos-completed" onClick={() => this.markCompleted(todo.id)}>✓</button>
+                                </li>
+                            );
+                        } else {
+                            return (
+                                <li className="todos-list-group-item" key={todo.id}>
+                                    {todo.value}
+                                    <button className="todos-completed" onClick={() => this.markCompleted(todo.id)}>✓</button>
+                                </li>
+                            );
+                        }
+                    })}
+                </ul>
             </div>
         );
     }
