c4e21048227fbfe28071e65a0cd552558f6e3faa
Weslley Braga <wesbragagt@gmail.com>
2019-06-22 10:10:42 -0500

    changed className in DashBTN

--- client/src/Components/DashBtn/DashBtn.js
+++ client/src/Components/DashBtn/DashBtn.js
@@ -3,9 +3,9 @@ import "./DashBtn.scss";
 export default function DashBtn() {
     return (
         <div className="dashbtn">
-            <button className="btn">Calendar</button>
-            <button className="btn">Bands</button>
-            <button className="btn">Contact</button>
+            <button>Calendar</button>
+            <button>Bands</button>
+            <button>Contact</button>
         </div>
     );
 }
