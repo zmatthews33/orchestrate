89b619f0f10e598069407deaddd69092ccadd675
Weslley Braga <wesbragagt@gmail.com>
2019-06-21 22:11:28 -0500

    added upcoming events box with placeholder data and container component to style grid

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Container from "../Components/Container/Container";
 import Todos from "../Components/Todos/Todos";
+import UpcomingEvents from "../Components/UpcomingEvents/UpcomingEvents";
+import "./Home.scss";
 function Home() {
-    return <Todos />;
+    return (
+        <Container>
+            <UpcomingEvents />
+            <Todos />
+        </Container>
+    );
 }
 
 export default Home;
