72dc85970f28bdb63b2905c85587bf210fe32a6d
Weslley Braga <wesbragagt@gmail.com>
2019-06-23 23:05:35 -0500

    basic layout for login

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,69 +1,56 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
-import {Page} from '../Components/Containers/LayoutsElements'
-import { Container, Row, Col, Card } from "reactstrap";
-import { Button, Form, FormGroup, Label, Input, Badge } from "reactstrap";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Page } from "../Components/Containers/LayoutsElements";
+import "../Styles/Login.scss";
+const style = {
+    border: "solid black 2px",
+    padding: "1rem",
+    width: "400px",
+    height: "400px",
+    margin: "0 auto",
+    position: "relative",
+    top: "35vh",
+    display: "flex",
+    flexDirection: "column"
+};
 function Login({ login }) {
-  const [input, setInput] = useState('')
-  const [password, setPassword] = useState('')
+    const [input, setInput] = useState("");
+    const [password, setPassword] = useState("");
 
-  const submitLogin = () => {
-    // add axios api call...
-    console.log('submitted...')
-  }
+    const submitLogin = () => {
+        // add axios api call...
+        console.log("submitted...");
+    };
 
-  return (
-
-    <Page>
-      <Container fluid className="login-container">
-        <Row className="login-form">
-          <Col xs={{ size: 12 }}>
-            <Card body className="login-card">
-              <Form>
-                <Row>
-                  <Col sm="12">
-                    <FormGroup>
-                      <Label for="login">Email:</Label>
-                      <Input
-                        type="email"
+    return (
+        <Page>
+            <form className="login-form">
+                <div className="login-form-group">
+                    <label for="email">Email:</label>
+                    <input
+                        type="text"
+                        placeholder="johndoe@ie.com"
                         name="email"
-                        id="Email"
-                        placeholder="metalmania@motorhead.com"
-                      />
-                    </FormGroup>
-                  </Col>
-                </Row>
-
-                <Row>
-                  <Col sm="12">
-                    <FormGroup>
-                      <Label for="password">Password:</Label>
-                      <Input
+                    />
+                </div>
+                <div className="login-form-group">
+                    <label for="password">Password:</label>
+                    <input
                         type="password"
+                        placeholder="Enter Password"
                         name="password"
-                        id="Password"
-                        placeholder="insert password"
-                      />
-                    </FormGroup>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col sm="12" className="text-right">
-                    <Button>Sign in</Button>
-                  </Col>
-                  <Col sm="12" className="text-center pt-4">
-                  <Link to="/signup"><Badge>Sign up for an account</Badge></Link>
-                    
-                  </Col>
-                </Row>
-              </Form>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-      
-    </Page>
-  )
+                    />
+                </div>
+
+                <div className="login-form-action">
+                    <button className="btn-submit" type="submit">
+                        Sign in
+                    </button>
+                    <Link className="signup-now" to="/signup">Sign up for an account</Link>
+                </div>
+            </form>
+        </Page>
+    );
 }
 
-export default Login
+export default Login;
