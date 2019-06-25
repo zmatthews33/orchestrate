0d5fe7937f615c5999e5284556de7db811b12831
Weslley Braga <wesbragagt@gmail.com>
2019-06-25 12:20:53 -0500

    handling multiple input changes

--- client/src/Components/ContactList/AddContact.js
+++ client/src/Components/ContactList/AddContact.js
@@ -6,36 +6,83 @@ export default function AddContact({
     venue,
     address,
     email,
-    note,
+    notes,
     handleContactSubmit,
     ...props
 }) {
+    const [userInput, setUserInput] = useReducer(
+        (state, newState) => ({ ...state, ...newState }),
+        {
+            name: "",
+            phone: "",
+            venue: "",
+            address: "",
+            email: "",
+            note: ""
+        }
+    );
+
+    const handleInputChange = e => {
+        const { value, name } = e.target;
+        setUserInput({ [name]: value });
+        console.log(userInput);
+    };
     return (
         <div id="add-new-contact">
             <form onSubmit={handleContactSubmit}>
                 <div className="name-container">
                     <label>Name:</label>
-                    <input type="text" placeholder="Sherlock Holmes" />
+                    <input
+                        name="name"
+                        type="text"
+                        placeholder="Sherlock Holmes"
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <div className="phone-container">
                     <label>Phone:</label>
-                    <input type="number" placeholder="###-###-####" />
+                    <input
+                        name="phone"
+                        type="text"
+                        placeholder="###-###-####"
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <div className="venue-container">
                     <label>Venue:</label>
-                    <input type="text" placeholder="the showdown club" />
+                    <input
+                        name="venue"
+                        type="text"
+                        placeholder="the showdown club"
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <div className="address-container">
                     <label>Address:</label>
-                    <input type="text" placeholder="21 Baker Street" />
+                    <input
+                        name="address"
+                        type="text"
+                        placeholder="21 Baker Street"
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <div className="email-container">
                     <label>Email:</label>
-                    <input type="email" placeholder="email@email.com" />
+                    <input
+                        name="email"
+                        type="email"
+                        placeholder="email@email.com"
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <div className="notes-container">
                     <label>Notes:</label>
-                    <input type="text" placeholder="add a note" />
+                    <input
+                        name="notes"
+                        type="text"
+                        placeholder="add a note"
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <div className="btn-container">
                     <button>Submit</button>
