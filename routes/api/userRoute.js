6fa645bc578e9b93e53dfddb000b0e7e72da82f0
jstevens79 <jstevens79@users.noreply.github.com>
2019-06-30 15:43:39 -0500

    creating login and register routes

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -1,20 +1,17 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
-const passport = require("passport"); 
 
 // Matches with "/api/user"
-router.route("/")
-    .get(userController.findAll)
-    .post(userController.create);
+router
+  .route("/")
+  .get(userController.findAll)
+  .post(userController.create);
 
 // Matches with "/api/user/:id"
 router
-    .route("/:id")
-    .get(userController.findById)
-    .put(userController.update)
-    .delete(userController.remove);
+  .route("/:id")
+  .get(userController.findById)
+  .put(userController.update)
+  .delete(userController.remove);
 
 module.exports = router;
