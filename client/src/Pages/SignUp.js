bb803c305d0b64187d36aba88d84fbed4035ecc7
Weslley Braga <wesbragagt@gmail.com>
2019-06-30 09:00:08 -0500

    handling sign up form in the database

--- client/src/Pages/SignUp.js
+++ client/src/Pages/SignUp.js
@@ -1,6 +1,7 @@
 import React, { useState, useReducer } from "react";
 import { Page } from "../Components/Containers/LayoutsElements";
-
+import axios from "axios";
+import useAPI from "../Utils/useAPI";
 function SignUp() {
     const [userInput, setUserInput] = useReducer(
         (state, newState) => ({ ...state, ...newState }),
@@ -19,6 +20,14 @@ function SignUp() {
 
         setUserInput({ [name]: newValue });
     };
+    const checkExist = (input, arr) => {
+        const exists = Object.keys(arr).some(k => arr[k] === input.value);
+
+        // evaluate if an email doesn't already exists
+        exists
+            ? console.log(`this ${input.name} is already being used`)
+            : console.log(`this ${input.name} is available to use`);
+    };
     const handleSubmit = e => {
         e.preventDefault();
         const { first_name, last_name, nickname, email, password } = e.target;
@@ -27,11 +36,33 @@ function SignUp() {
             first_name: first_name.value,
             last_name: last_name.value,
             nickname: nickname.value,
-            email: email.value,
+            email: [{ email: email.value }],
             password: password.value
         };
-        console.log(newUser);
+        // console.log(newUser);
         // submit new user to api call and add to user model
+        axios
+            .get("/api/user")
+            .then(response => {
+                // find in that response body if this user already exists
+                const dataBaseEmails = response.data.map(
+                    data => data.email[0].email
+                );
+
+                // console.log('database emails', dataBaseEmails);
+                // console.log('target email', email.value);
+
+                checkExist(email, dataBaseEmails);
+            })
+            .then(() =>
+                axios
+                    .post("api/user", newUser)
+                    .then(user =>
+                        console.log(user)
+                    )
+                    .catch(err => console.log(err))
+            )
+            .catch(err => console.log(err));
     };
     return (
         <Page>
--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-<<<<<<< HEAD
   first_name: { type: String, required: true },
   last_name: { type: String, require: true },
   nickname: String,
@@ -34,33 +33,6 @@ const userSchema = new Schema({
   role: String,
   position: String,
   profile_img: String
-=======
-    first_name: { type: String, required: true },
-    last_name: { type: String, require: true },
-    nickname: String,
-    address: {
-        address_line1: String,
-        address_line2: String,
-        city: String,
-        state: String,
-        zip_code: String
-    },
-    email: [{ email_type: String, email: { type: String, required: true }, isPrimary: Boolean }],
-    phone: [{ phone_type: String, phone_num: String, isPrimary: Boolean }],
-    time_zone: String,
-    gender: String,
-    dob: Date,
-    social_accounts: {
-        facebook: String,
-        twitter: String,
-        instagram: String
-    },
-    password: { type: String, require: true },
-    lastLogin: Date,
-    role: String,
-    position: String,
-    profile_img: String
->>>>>>> 9d79be2d6da6a1223e49c092665546289b971ab9
 });
 
 const User = mongoose.model("User", userSchema);
