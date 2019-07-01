a5dea5072e3c78871762805188fd58062c43c1cc
jstevens79 <jstevens79@users.noreply.github.com>
2019-06-30 19:35:39 -0500

    removing unused package

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   first_name: { type: String, required: true },
