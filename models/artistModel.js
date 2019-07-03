9bfeb66ee1df7fbb6789122a8d61f9b54f770b42
Weslley Braga <wesbragagt@gmail.com>
2019-07-02 20:00:13 -0500

    reading bands from database

--- models/artistModel.js
+++ models/artistModel.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema;
 const artistSchema = new Schema(
     {
         name: { type: String, required: true },
-        artist_type: { type: String, required: true },
+        // artist_type: { type: String, required: true },
         members: [{ type: Schema.Types.ObjectId, ref: "Person" }],
-        genres: [{ type: String, require: true }],
-        isActive: { type: Boolean, require: true },
+        genres: [{ type: String, require: false }],
+        isActive: { type: Boolean, require: false },
         profile_img: String,
         team: [{ type: Schema.Types.ObjectId, ref: "Person" }],
         created_by: { type: Schema.Types.ObjectId, ref: "User" }
