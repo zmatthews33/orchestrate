acc459f42f3902c7d8ee16feed570c30c1e76f8a
jstevens79 <jstevens79@users.noreply.github.com>
2019-07-05 23:22:01 -0500

    making some state flow tweaks

--- client/src/Components/ContactList/AddContact.js
+++ client/src/Components/ContactList/AddContact.js
@@ -3,10 +3,12 @@ import axios from "axios";
 
 import "./AddContact.scss";
 
+
 export default function AddContact({ userId, addContact }) {
 
   const InitState = {
-    name: "",
+    first_name: "",
+    last_name: "",
     phone: "",
     venue: "",
     address: "",
@@ -21,19 +23,18 @@ export default function AddContact({ userId, addContact }) {
   const [State, setState] = useReducer(reducer, InitState);
 
   useEffect(() => {
-    if (userId) setState({ crearted_by: userId })
+    if (userId) setState({ created_by: userId })
   }, [userId])
 
-  const handleInputChange = event => {
-    let { value, name } = event;
-    setState({ [name]: value });
+  const handleInputChange = e => {
+    const keyVal = e.target.name;
+    const inputVal = e.target.value;
+    setState({ [keyVal]: inputVal });
   };
 
   const handleSubmit = e => {
     e.preventDefault();
-    axios.post("/api/people", State)
-      .then(response => addContact(response.data))
-      .catch(err => console.log(err))
+    addContact(State)
   };
 
   return (
@@ -43,70 +44,70 @@ export default function AddContact({ userId, addContact }) {
         <div className="formGroup">
           <label>First Name:</label>
           <input
+            placeholder="Sherlock"
             name="first_name"
             type="text"
-            placeholder="Sherlock"
-            onChange={e => handleInputChange(e.target)}
+            onChange={e => handleInputChange(e)}
             value={State.first_name}
           />
         </div>
-        <div className="lastname-container">
+        <div className="formGroup">
           <label>Last Name:</label>
           <input
+            placeholder="Holmes"
             name="last_name"
             type="text"
-            placeholder="Holmes"
-            onChange={e => handleInputChange(e.target)}
+            onChange={e => handleInputChange(e)}
             value={State.last_name}
           />
         </div>
-        <div className="phone-container">
+        <div className="formGroup">
           <label>Phone:</label>
           <input
             name="phone"
             type="text"
             placeholder="###-###-####"
-            onChange={e => handleInputChange(e.target)}
+            onChange={e => handleInputChange(e)}
             value={State.phone}
           />
         </div>
-        <div className="venue-container">
+        <div className="formGroup">
           <label>Venue:</label>
           <input
             name="venue"
             type="text"
             placeholder="the showdown club"
-            onChange={e => handleInputChange(e.target)}
+            onChange={e => handleInputChange(e)}
             value={State.venue}
           />
         </div>
-        <div className="address-container">
+        <div className="formGroup">
           <label>Address:</label>
           <input
             name="address"
             type="text"
             placeholder="21 Baker Street"
-            onChange={e => handleInputChange(e.target)}
+            onChange={e => handleInputChange(e)}
             value={State.address}
           />
         </div>
-        <div className="email-container">
+        <div className="formGroup">
           <label>Email:</label>
           <input
             name="email"
             type="email"
             placeholder="email@email.com"
-            onChange={e => handleInputChange(e.target)}
+            onChange={e => handleInputChange(e)}
             value={State.email}
           />
         </div>
-        <div className="notes-container">
+        <div className="formGroup">
           <label>Notes:</label>
           <input
             name="note"
             type="text"
             placeholder="add a note"
-            onChange={e => handleInputChange(e.target)}
+            onChange={e => handleInputChange(e)}
             value={State.note}
           />
         </div>
@@ -114,7 +115,7 @@ export default function AddContact({ userId, addContact }) {
           <button className="btn-submit" type="submit">Submit</button>
         </div>
       </form>
-    </div >
+    </div>
   );
 
 };
