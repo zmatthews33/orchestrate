7e0f740418ea0bcc62512fa53c817b0e876f5a16
Weslley Braga <wesbragagt@gmail.com>
2019-07-06 00:01:36 -0500

    changes to contact list

--- client/src/Components/ContactList/ContactList.js
+++ client/src/Components/ContactList/ContactList.js
@@ -1,43 +1,11 @@
-import React, { useState, useEffect, useContext } from "react";
-import axios from "axios";
+import React from "react";
+
 // import AddContact from "./AddContact";
 import ContactTable from "./ContactTable";
 import "./ContactList.scss";
-import { AppContext } from "../../App";
+
 
 function ContactList({ contacts, deleteContact }) {
-  // const handleContactSubmit = e => {
-  //   e.preventDefault();
-  //   console.log(e.target);
-  //   const {
-  //     first_name,
-  //     last_name,
-  //     phone,
-  //     venue,
-  //     address,
-  //     email,
-  //     note
-  //   } = e.target;
-
-  //   const newContact = {
-  //     first_name: first_name.value,
-  //     last_name: last_name.value,
-  //     phone: phone.value,
-  //     venue: venue.value,
-  //     address: address.value,
-  //     email: email.value,
-  //     note: note.value,
-  //     created_by: userId
-  //   };
-  //   console.log(newContact);
-  //   // setContacts([newContact, ...contacts]);
-  //   // insert api call to add a new contact to personal list
-  //   axios
-  //     .post("api/people", newContact)
-  //     .then(response => setContacts([response.data, ...contacts]));
-  // };
-
-  // console.log("All contacts", contacts);
   return (
     <div className="contact-list">
       {/* Reading from state */}
