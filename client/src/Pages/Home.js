09a3c148b5f2d1a8902fa145da0d5dea994de789
jstevens79 <jstevens79@users.noreply.github.com>
2019-07-05 20:12:52 -0500

    reorganizing components a bit

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import {Page} from "../Components/Containers";
-// import TodoList from "../Components/TodoList/TodoList";
-// import UpcomingEvents from "../Components/UpcomingEvents/UpcomingEvents";
-// import Finances from "../Components/Finances/Finances";
 import "../Styles/Home.scss";
+import {AppContext} from '../App';
+import useAPI from '../Utils/useAPI'
+import TodoList from "../Components/TodoList/TodoList";
+
 
 function Home() {
+  //const {userId} = useContext(AppContext)
+  // const getEventsData = useAPI("get", `/api/event?created_by=${userId}`);
+  // const getArtistsData = useAPI("get", `/api/artist?created_by=${userId}`);
+  // const getReminders = useAPI("get", `/api/todo?created_by=${userId}`);
+  
+
   return (
     <Page>
-      
+      <div className="topFeature"></div>
+      <div className="lowerFeatures">
+        <div className="lowerLeft"></div>
+        <div className="lowerRight">
+          <TodoList />
+        </div>
+      </div>
     </Page>
   );
 }
 
 export default Home;
-
-// <div className="Feature">
-//         <UpcomingEvents />
-//       </div>
-//       <TodoList />
-//       <Finances />
